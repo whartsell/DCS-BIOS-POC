154df7d30b01bd9262553654e0ebddb6960be515
Will Hartsell <will@willshouse.net>
2015-12-17 06:57:43 -0600

    initial commit of IOT solution

---
+++ DCS-BIOS-IOT/GaugeLib/Game1.cs
@@ -0,0 +1,172 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using net.willshouse.dcs.dcsbios;
+using System;
+
+namespace net.willshouse.dcs.gaugelib
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        private Manager manager;
+        private string address = "239.255.50.10";
+        private int port = 5010;
+        private Needle needle;
+        private GaugeFace face;
+        private bool test;
+        private int testValue;
+        private KeyboardState prevState;
+        private Color background;
+        private Color defaultBackground;
+        private Listener listener;
+        public event Action StartEvent;
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            
+           
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            manager = new Manager();
+            manager.Start();
+            test = false;
+            testValue = 0;
+            background = Color.CornflowerBlue;
+            defaultBackground = Color.CornflowerBlue;
+            listener = new Listener(manager);
+            this.StartEvent += listener.Start;
+            Action startEvent = StartEvent;
+            if (startEvent != null) startEvent();
+            
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            int x = GraphicsDevice.Viewport.Width / 2;
+            int y = GraphicsDevice.Viewport.Height / 2;
+            face = new GaugeFace(Content.Load<Texture2D>("resources/faceplate"), new Vector2(0, 0));
+            needle = new Needle(Content.Load<Texture2D>("resources/needle"), new Vector2(150, 150));
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            int value;
+            KeyboardState state = Keyboard.GetState();
+
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || state.IsKeyDown(Keys.Escape))
+                Shutdown();
+
+           if ((state.IsKeyDown(Keys.F1)) && (prevState.IsKeyUp(Keys.F1)))
+            {
+                if (!test)
+                {
+                    //manager.Stop();
+                    listener.Stop();
+                    test = true;
+                    background = Color.Red;
+                    
+                    
+                }
+                else
+                {
+                    //manager.Start();
+                    listener.Start();
+                    test = false;
+                    background = defaultBackground; ;
+                }
+
+                
+            }
+             prevState = state;
+
+            // TODO: Add your update logic here
+            if (test)
+            {
+                if (state.IsKeyDown(Keys.Right))
+                    testValue +=100;
+                else if (state.IsKeyDown(Keys.Left))
+                    testValue -=100;
+                value = testValue;
+            }
+            else
+            {
+                value = manager.getDataAtAddress(0x107a);
+                //value = 32767;
+            }
+            needle.Update(value);
+           
+      
+            if (listener.Pulse)
+            {
+                background = Color.Green;
+            } else
+            {
+                background = Color.CornflowerBlue;
+            }
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(background);
+            spriteBatch.Begin();
+            face.Draw(spriteBatch);
+            needle.Draw(spriteBatch);
+            spriteBatch.End();
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+
+        protected void Shutdown()
+        {
+            //manager.Stop();
+            listener.Stop();
+            Exit();
+        }
+
+        
+    }
+}
---
+++ DCS-BIOS-IOT/Gauges-IOT/listener.cs
@@ -0,0 +1,100 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Networking;
+using Windows.Networking.Sockets;
+using Windows.Storage.Streams;
+using System.Net.Sockets;
+using System.Net;
+
+namespace net.willshouse.dcs.dcsbios
+{
+    class Listener
+    {
+
+        //private Socket s;
+        private DateTime prevTime;
+        private int delay;
+        private Manager manager;
+        private HostName address;
+        private string port;
+        private DatagramSocket socket;
+        private IOutputStream outputStream;
+        //private UdpSocketMulticastClient receiver;
+        public bool Pulse { get; set; }
+        public Listener(Manager manager)
+        {
+            prevTime = DateTime.Now;
+            Pulse = false;
+            delay = 1;
+            this.manager = manager;
+            
+            address = new HostName(manager.Address);
+            port = manager.Port.ToString();
+            socket = new DatagramSocket();
+        }
+
+         async public void Start()
+        {
+            IPAddress local = IPAddress.Parse("192.168.1.2");
+            //s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            //s.Bind(new IPEndPoint(local, 5010));
+            
+            //mcastOption = new MulticastOption(manager.Address, local);
+            socket.MessageReceived += messageReceived;
+            socket.Control.MulticastOnly = true;
+            await socket.BindServiceNameAsync(port);
+            socket.JoinMulticastGroup(address);
+            outputStream = await socket.GetOutputStreamAsync(address, port);
+            const string stringToSend = "Hello";
+            DataWriter writer = new DataWriter(outputStream);
+            writer.WriteString(stringToSend);
+            await writer.StoreAsync();
+           
+        }
+
+        public void Stop()
+        {
+            socket.Dispose();
+        }
+
+         public void messageReceived(object s,DatagramSocketMessageReceivedEventArgs e)
+        {
+
+            DataReader message = e.GetDataReader();
+            while (message.UnconsumedBufferLength > 0)
+            {
+                manager.Parser.processChar(message.ReadByte());
+            }
+            Send();
+
+        }
+
+        async private void Send()
+        {
+            outputStream = await socket.GetOutputStreamAsync(address, port);
+            const string stringToSend = "Hello";
+            DataWriter writer = new DataWriter(outputStream);
+            writer.WriteString(stringToSend);
+            await writer.StoreAsync();
+        }
+        public void ping()
+        {
+
+            DateTime curTime = DateTime.Now;
+            TimeSpan elapsed = new TimeSpan(DateTime.Now.Ticks - prevTime.Ticks);
+            if (elapsed.Seconds > delay)
+            {
+                prevTime = curTime;
+                Pulse = !Pulse;
+            }
+            
+
+            
+        }
+        
+    }
+}
